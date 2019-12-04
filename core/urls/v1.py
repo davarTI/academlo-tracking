74c525464c676a81dd5b6227465eec95fd0b73ba
Nicolas Rondon <nicolasrondon@outlook.es>
2019-12-04 16:02:39 -0500

    ADD model, serializer, viewset projects

--- core/urls/v1.py
+++ core/urls/v1.py
@@ -4,10 +4,12 @@ from rest_framework import routers
 from core.views import UserViewSet
 # from academlo_tracking.views import BoardViewSet
 from workspaces.views import WorkspaceViewSet
+from projects.views import ProjectViewSet
 
 routers = routers.DefaultRouter()
 routers.register(r'users', UserViewSet)
 routers.register(r'workspaces', WorkspaceViewSet)
+routers.register(r'projects', WorkspaceViewSet)
 # routers.register(r'boards', BoardViewSet)
 
 urlpatterns = [
---
+++ projects/models.py
@@ -0,0 +1,15 @@
+from datetime import timezone
+
+from django.db import models
+
+
+# Create your models here.
+from workspaces.models import Workspace
+
+
+class Project(models.Model):
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=350)
+    created_at = models.DateTimeField(auto_now_add=timezone.now())
+    delete_at = models.DateTimeField(auto_now_add=timezone.now())
